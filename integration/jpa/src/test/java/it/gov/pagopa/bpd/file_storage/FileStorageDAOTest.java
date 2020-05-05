2e0247a8773557a9f440cbeb518e96bd497af99c
mdesogus <michael.desogus@everis.com>
2020-05-05 17:13:07 +0200

    fixing code style

--- integration/jpa/src/test/java/it/gov/pagopa/bpd/file_storage/FileStorageDAOTest.java
+++ integration/jpa/src/test/java/it/gov/pagopa/bpd/file_storage/FileStorageDAOTest.java
@@ -25,7 +25,7 @@ public class FileStorageDAOTest extends BaseCrudJpaDAOTest<FileStorageDAO, FileS
     }
 
     @Override
-    protected FileStorageDAO getCitizenDAO() {
+    protected FileStorageDAO getDao() {
         return fileStorageDAOMock;
     }
 
